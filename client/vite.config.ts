a2d77636cdb32db23c0b9153f3705dcdc3d9ac2d
maxime.laffaire <maxoulaf@orange.fr>
2026-09-24 13:30:20 +0200

    Changement d'icone

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.png'],
       manifest: {
         name: 'EventCo',
         short_name: 'EventCo',
